19438ed4a7c24c5de30e5ba68282342838a2eec9
Nicolessi Nathan <nathan.nicolessi@etu.univ-orleans.fr>
2025-05-01 11:21:57 +0200

    Question 8

--- dockerumbutu/dev/tp_hello/src/Controller/HelloController.php
+++ dockerumbutu/dev/tp_hello/src/Controller/HelloController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     #[Route('/hello', name: 'app_hello')]
     public function hello():Response{
-        return new Response('Mon message');
+        return $this->render('hello/index.html.twig');
     }
 
     #[Route('/', name: 'app_home')]
